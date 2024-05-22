0ecaf13fd15ff3e89ce2c1cbc37e87285b5bb480
NgocToan33 <ngongoctoan2005nt@gmail.com>
2024-05-22 15:26:04 +0700

    ok

--- Assets/Script/Player Move.cs
+++ Assets/Script/Player Move.cs
@@ -5,18 +5,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
+
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpSpeed = 10f;
     private Rigidbody2D _rigidbody2D;
     Vector2 moveInput;
+    private Animator _anmator;
+    CapsuleCollider2D _capsuleCollider2D; 
+    
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _anmator = GetComponent<Animator>();
+        _capsuleCollider2D = GetComponent<CapsuleCollider2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
+
         Run();
         FlipSprite();
     }
@@ -32,9 +40,15 @@ public class PlayerMove : MonoBehaviour
         // (0,-1) Down
     }
 
-    void OnJump()
+    void OnJump(InputValue value) 
     {
-        Debug.Log(message:">>>>> Jump");
+        var isTouchingGroud = _capsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        if (!isTouchingGroud) return;
+        if(value.isPressed)
+        {
+            Debug.Log(message: ">>>>> Jump");
+            _rigidbody2D.velocity += new Vector2(x: 0, y: jumpSpeed);
+        }
     }
 
     // dieu khien chuyen dong nhan vat
@@ -42,6 +56,9 @@ public class PlayerMove : MonoBehaviour
     {
         var moveVelocity = new Vector2(x:moveInput.x * moveSpeed, _rigidbody2D.velocity.y);
         _rigidbody2D.velocity = moveVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(moveInput.x) > Mathf.Epsilon;
+        _anmator.SetBool(name:"isRuning", playerHasHorizontalSpeed);
     }
 
     // Abs: gia tri tuyet doi 
